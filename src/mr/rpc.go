ee6166800cae37bf5799ab66486ac5ed73d662e9
zhihao-gong <gzhapply@outlook.com>
2023-11-18 02:06:30 +0000

    Add registryWoker/ping signature

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,6 +24,23 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type RegisterArgs struct {
+}
+
+type RegisterReply struct {
+	code int
+	assgnedId int
+	message string
+}
+
+type PingArgs struct {
+	assgnedId int
+}
+
+type PingReply struct {
+	code int
+	message string
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
