9f5c4981c71657926660b322035623fbab91c8c4
zhihao-gong <gzhapply@outlook.com>
2023-12-02 20:44:44 +0800

    Update

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,40 +1,84 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
 
+	"github.com/google/uuid"
+)
 
+type worker struct {
+	Id           string
+	LastPingTime int64
+	status       WorkerStatus
+}
+
+type MapTask struct {
+	Id       string
+	FileName string
+}
+
+type ReduceTask struct {
+	Id string
+}
+
+// Coordinator holds all the information about the current state of the map reduce job
 type Coordinator struct {
-	// Your definitions here.
+	WorkerMutex sync.RWMutex
+	Workers     map[string]worker
+
+	MapTaskMutex sync.RWMutex
+	MapTasks     map[string]MapTask
+
+	ReduceTaskMutex sync.RWMutex
+	ReduceTasks     map[string]ReduceTask
+
+	KeepAliveTheshold int64
 }
 
-// Your code here -- RPC handlers for the worker to call.
+// RegisterWorker generates a unique ID for a new worker, assigns it to the worker and returns the ID
+func (c *Coordinator) Register(args *RegisterArgs, reply *RegisterReply) error {
+	assignedId := uuid.New().String()
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+	c.WorkerMutex.Lock()
+	c.Workers[assignedId] = worker{
+		Id:           assignedId,
+		LastPingTime: time.Now().Unix(),
+		status:       Idle,
+	}
+	c.WorkerMutex.Unlock()
+
+	reply.Code = 0
+	reply.AssgnedId = assignedId
+	reply.Message = "Registered"
+
 	return nil
 }
 
+// Ping is called by the worker to let the coordinator know that it is still alive
+// func (c *Coordinator) Ping(args *PingArgs, reply *PingReply) error {
+// 	workerId := args.AssignedId
 
-func (c *Coordinator) RegisterWorker(args *RegisterArgs, reply *RegisterReply) error {
-	return nil
-}
+// 	c.WorkerMutex.Lock()
+// 	defer c.WorkerMutex.Unlock()
 
-func (c *Coordinator) Ping(args *PingArgs, reply *PingReply) error {
-	return nil
-}
+// 	worker, ok := c.Workers[workerId]
+// 	if !ok {
+// 		return errors.New("worker not found for id: " + workerId)
+// 	}
 
-//
+// 	worker.LastPingTime = time.Now().Unix()
+// 	c.Workers[workerId] = worker
+
+// 	return nil
+// }
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -48,29 +92,25 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-	// Your code here.
-
+	c := Coordinator{
+		KeepAliveTheshold: 10,
+		Workers:           make(map[string]worker),
+		WorkerMutex:       sync.RWMutex{},
+	}
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,78 +1,83 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
 
+type WorkerStatus int
 
-//
+const (
+	Idle WorkerStatus = iota
+	InProgress
+	Completed
+)
+
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type myworker struct {
+	AssignedId string
+	status     WorkerStatus
+}
+
+func (w *myworker) Report(args *ReportArgs, reply *ReportReply) error {
+	return nil
+}
+
+func (w *myworker) AskForTask(args *AskForTaskArgs, reply *AskForTaskReply) error {
+	return nil
+}
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	assignedId := Register()
 
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
+	worker := myworker{
+		AssignedId: assignedId,
+		status:     Idle,
 	}
+	log.Println("Worker registered with id: ", worker.AssignedId)
 }
 
-//
+// Register worker on the corrdinator side and get the assigned id
+func Register() string {
+	args := RegisterArgs{}
+	reply := RegisterReply{}
+
+	ok := call("Coordinator.Register", &args, &reply)
+
+	if !ok {
+		log.Fatal("Register error")
+	}
+	if reply.Code != 0 {
+		log.Fatal("Register error: ", reply.Message)
+	}
+
+	return reply.AssgnedId
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
