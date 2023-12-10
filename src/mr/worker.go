5c7eaa2d7d29a49dcecf701653401fd26e56da80
zhihao-gong <gzhapply@outlook.com>
2023-12-10 13:34:18 +0800

    Add register

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -5,6 +5,7 @@ import (
 	"hash/fnv"
 	"log"
 	"net/rpc"
+	"time"
 )
 
 type WorkerStatus int
@@ -15,6 +16,14 @@ const (
 	Completed
 )
 
+type TaskType int
+
+const (
+	MapTaskType TaskType = iota
+	ReduceTaskType
+	ShuffleTaskType
+)
+
 // Map functions return a slice of KeyValue.
 type KeyValue struct {
 	Key   string
@@ -22,15 +31,95 @@ type KeyValue struct {
 }
 
 type myworker struct {
-	AssignedId string
-	status     WorkerStatus
+	assignedId  string
+	status      WorkerStatus
+	mapFunc     func(string, string) []KeyValue
+	reduceFunc  func(string, []string) string
+	shuffleFunc func(string, []string) string
+
+	pendingTasks SafeQueue
+	finishedTask SafeQueue
 }
 
-func (w *myworker) Report(args *ReportArgs, reply *ReportReply) error {
-	return nil
+// Task is the unit of work for the worker
+type Task struct {
+	Id     string
+	Input  string
+	Output string
+	Type   TaskType
 }
 
-func (w *myworker) AskForTask(args *AskForTaskArgs, reply *AskForTaskReply) error {
+// Register worker on the corrdinator side and get the assigned id
+func (w *myworker) register() string {
+	args := RegisterArgs{}
+	reply := RegisterReply{}
+
+	ok := call("Coordinator.Register", &args, &reply)
+
+	if !ok {
+		log.Fatal("Register error")
+	}
+	if reply.Code != 0 {
+		log.Fatal("Register error: ", reply.Message)
+	}
+
+	return reply.AssgnedId
+}
+
+// Report the status of the worker and task to the coordinator
+func (w *myworker) AskForTask() {
+	args := AskForTaskArgs{}
+	reply := AskForTaskReply{}
+
+	ok := call("Coordinator.Report", &args, &reply)
+	if !ok {
+		log.error("Report error")
+		return
+	}
+
+	if reply.Code != 0 {
+		log.error("Report error: ", reply.Message)
+		return
+	}
+
+	pendingTasks.Enqueue(reply.Task)
+}
+
+func (w *myworker) DoTask() {
+	for {
+		task := pendingTasks.Dequeue()
+		if task == nil {
+			sleep(10 * time.Second)
+		}
+
+		task.Input
+		switch task.Type {
+		case MapTaskType:
+			w.doMapTask(task)
+		case ReduceTaskType:
+			w.doReduceTask(task)
+		case ShuffleTaskType:
+			w.doShuffleTask(task)
+		}
+	}
+}
+
+// Start the worker and keep reporting the status to the coordinator
+func (w *myworker) Start() error {
+
+	w.assignedId = w.register()
+
+	timer := time.NewTicker(10 * time.Second)
+
+	go func() {
+		for {
+			select {
+			case <-timer.C:
+				w.AskForTask()
+			}
+		}
+	}()
+
 	return nil
 }
 
@@ -48,30 +137,12 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// Your worker implementation here.
 
-	assignedId := Register()
-
 	worker := myworker{
-		AssignedId: assignedId,
 		status:     Idle,
+		mapFunc:    mapf,
+		reduceFunc: reducef,
 	}
-	log.Println("Worker registered with id: ", worker.AssignedId)
-}
-
-// Register worker on the corrdinator side and get the assigned id
-func Register() string {
-	args := RegisterArgs{}
-	reply := RegisterReply{}
-
-	ok := call("Coordinator.Register", &args, &reply)
-
-	if !ok {
-		log.Fatal("Register error")
-	}
-	if reply.Code != 0 {
-		log.Fatal("Register error: ", reply.Message)
-	}
-
-	return reply.AssgnedId
+	worker.Start()
 }
 
 // send an RPC request to the coordinator, wait for the response.
