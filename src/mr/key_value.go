b595037b4155a06e48f0c25f1c25bedcf776fb37
zhihao-gong <gzhapply@outlook.com>
2024-02-08 11:42:07 +0800

    Refactor

---
+++ src/mr/key_value.go
@@ -0,0 +1,14 @@
+package mr
+
+// KeyValue returned by map functions
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+// for sorting by key.
+type byKey []KeyValue
+
+func (a byKey) Len() int           { return len(a) }
+func (a byKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
